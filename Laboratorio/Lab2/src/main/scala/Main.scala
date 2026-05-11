3d63d2adc6f3f5019e8b14c05c73e075ed7468f3
SantosFarias10 <santos.farias@mi.unc.edu.ar>
2026-05-11 11:19:07 -0300

    {ADD} Ejercicio 6

--- Laboratorio/Lab2/src/main/scala/Main.scala
+++ Laboratorio/Lab2/src/main/scala/Main.scala
@@ -8,8 +8,8 @@ object Main {
     // ------------------------------------------------------------------
     // Paso 1: Cargar diccionarios
     // ------------------------------------------------------------------
-    // TODO (Ejercicio 2)
-    val dictionary: List[NamedEntity] = ???
+    // (Ejercicio 2)
+    val dictionary: List[NamedEntity] = Dictionary.loadAll()
 
     println(s"Diccionario cargado: ${dictionary.size} entidades.\n")
 
@@ -28,18 +28,40 @@ object Main {
     // ------------------------------------------------------------------
     // Paso 3: Detectar entidades y mostrar resultados por post
     // ------------------------------------------------------------------
-    // TODO (Ejercicios 3, 4 y 6):
+    // (Ejercicios 3, 4 y 6):
     //   Para cada post:
     //     1. Detectar entidades
     //     2. Formatear y mostrar el resultado
 
+    // Retornamos todas las entidades detectadas (usando flatMap) para usarlas en el Paso 4
+    // flatMap: aplica la función y luego convierte una lista de listas en una sola lista
+    val allDetectedEntities: List[NamedEntity] = allPosts.flatMap { 
+      case (url, titles) =>
+        println(s"\n--- Resultados para $url ---")
+      
+        titles.flatMap { title =>
+        // 1. Detectar entidades
+        val entities = Analyzer.detectEntities(title, dictionary)
+        
+        // 2. Formatear y mostrar el resultado
+        println(Formatters.formatNERResult(title, entities))
+        
+        // Devolvemos las entidades de este post para que flatMap las junte todas
+        entities
+      }
+    }
+
     // ------------------------------------------------------------------
     // Paso 4: Estadísticas globales
     // ------------------------------------------------------------------
-    // TODO (Ejercicios 5 y 6):
+    // (Ejercicios 5 y 6):
     //   1. Recolectar TODAS las entidades detectadas en todos los posts
-    //   2. Contar por tipo
-    //   3. Mostrar el resumen
-
+    //   (Esto se hizo en el paso 3 guardándolas en allDetectedEntities)
+    
+    // 2. Contar por tipo
+    val globalCounts = Analyzer.countByType(allDetectedEntities)
+    
+    // 3. Mostrar el resumen
+    println("\n" + Formatters.formatEntityStats(globalCounts))
   }
 }
